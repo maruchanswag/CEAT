691eae4166aa271d76493e3bce7512ec804d1212
Gonzalo Molina <o.gonzalo@hotmail.com>
2023-04-27 14:58:42 -0300

    add vite-plugin-pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,19 @@ import vue from '@vitejs/plugin-vue'
 import Unocss from 'unocss/vite'
 import { presetUno } from 'unocss'
 import AutoImport from 'unplugin-auto-import/vite'
+import Pages from 'vite-plugin-pages'
+import { resolve } from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [
 		vue(),
 		Unocss({ presets: [presetUno()] }),
 		AutoImport({ imports: ['vue'] }),
+		Pages(),
 	],
+	resolve: {
+		alias: {
+			'~': resolve('src'),
+		},
+	},
 })
