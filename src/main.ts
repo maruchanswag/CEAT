ca276cbb3311da0a27a32477b44df27c9edd8de7
Gonzalo Molina <o.gonzalo@hotmail.com>
2023-04-27 14:58:49 -0300

    chore: setup routes

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,7 @@ import { createApp } from 'vue'
 import '@unocss/reset/tailwind-compat.css'
 import 'virtual:uno.css'
 import App from './App.vue'
-
-createApp(App).mount('#app')
+import routes from '~pages'
+import { createRouter, createWebHistory } from 'vue-router'
+const router = createRouter({ history: createWebHistory(), routes })
+createApp(App).use(router).mount('#app')
