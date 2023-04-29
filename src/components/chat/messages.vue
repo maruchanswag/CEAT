8c6e6809d7d3175fccd08c9dbaeaf074d6922d9a
Gonzalo Molina <o.gonzalo@hotmail.com>
2023-04-29 14:27:55 -0300

    chore: setup message list component

---
+++ src/components/chat/messages.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+	import { Message } from '~/types'
+	import MessageItem from './message-item.vue'
+
+	type Props = {
+		messages: Message[]
+	}
+
+	const props = defineProps<Props>()
+
+</script>
+
+<template>
+	<section
+		v-auto-animate
+		class="flex flex-col w-full h-full p-2 gap-5 mt-3 rounded-md dark:bg-dark-700 dark:border-dark-700 bg-light-50 border-gray-100 border-2 overflow-y-auto"
+	>
+		<MessageItem v-for="message in props.messages" :message="message" />
+	</section>
+</template>
