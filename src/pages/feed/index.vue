0bc5eab3e0ec24d111fff53e5b1add72837de238
Gonzalo Molina <o.gonzalo@hotmail.com>
2023-04-29 15:44:12 -0300

    chore: use useMessages composable on feed page

--- src/pages/feed/index.vue
+++ src/pages/feed/index.vue
@@ -1,42 +1,17 @@
 <script setup lang="ts">
-	import { pusher } from '~/lib/pusher'
-	import { Message } from '~/types'
+	import { useMessages } from '~/composables/messages'
 	import Messages from '~/components/chat/messages.vue'
 	import Form from '~/components/chat/form.vue'
-	import IconMoon from '~/components/icon/moon.vue'
-	import IconSun from '~/components/icon/sun.vue'
-	type MessageResponse = {
-		message: Message
-	}
+	import ThemeSwitcher from '~/components/nav/theme-switcher.vue'
 
-	const channel = pusher.subscribe('noxy-sysl')
-	const messages = ref<Message[]>([])
-	const dark = useDark()
-	const toggleDarkMode = () => (dark.value = !dark.value)
-	onMounted(() => {
-		const bindChannel = () =>
-			channel.bind('message', (data: MessageResponse) => {
-				messages.value?.push(data.message)
-			})
-		bindChannel()
-	})
-
-	onUnmounted(() => channel.disconnect())
+	const { messages } = useMessages()
 </script>
 
 <template>
 	<main class="container mx-auto max-w-4xl h-screen">
-		<header
-			class=" p-5 flex flex-row justify-around items-center"
-		>
+		<header class="p-5 flex flex-row justify-around items-center">
 			<h2 class="text-center text-3xl font-bold self-start">Noxy - chat</h2>
-			<button
-				class="flex flex-row items-center justify-center h-8 w-8 dark:bg-transparent  bg-white self-end"
-				@click="toggleDarkMode()"
-			>
-				<IconSun />
-				<IconMoon />
-			</button>
+			<ThemeSwitcher />
 		</header>
 
 		<Messages :messages="messages" />
