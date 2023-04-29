d5f341fdacde9b0af71dc5ecabf3b3a5e0c4e647
Gonzalo Molina <o.gonzalo@hotmail.com>
2023-04-29 15:43:21 -0300

    chore: add theme-switcher component

---
+++ src/components/nav/theme-switcher.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+	import SunIcon from '../icon/sun.vue'
+	import MoonIcon from '../icon/moon.vue'
+	const dark = useDark()
+	const toggleDarkMode = () => (dark.value = !dark.value)
+</script>
+
+<template>
+	<button
+		class="flex flex-row items-center justify-center h-8 w-8 dark:bg-transparent bg-white self-end"
+		@click="toggleDarkMode()"
+	>
+		<SunIcon />
+		<MoonIcon />
+	</button>
+</template>
